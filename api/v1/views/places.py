000bd4f4fdf1763d00919897a02c43f206d8d5a6
klich1984 <2239@holbertonschool.com>
2021-05-10 11:48:23 -0500

    00 | add file | new view for Place objects that handles all default RestFul API actions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,9 +1,11 @@
-"""REST full view for cities objects"""
+#!/usr/bin/python3
+"""REST full view for places objects"""
 from flask import Flask, abort, jsonify, make_response
 from flask import request
 from api.v1.views import app_views
 from models.city import City
 from models.place import Place
+from models.user import User
 from models import storage
 
 
@@ -42,3 +44,50 @@ def place_get_id(place_id=None):
                 storage.delete(place_objs)
                 return make_response(jsonify({}), 200)
     abort(404)
+
+
+@app_views.route("/cities/<city_id>/places", methods=["POST"])
+def place_create(city_id=None):
+    """Creates a places within a cities by its id"""
+
+    # states_dict = storage.all(State)
+    # places_dict = storage.all(Place)
+    # cities_list = []
+
+    my_cities_obj = storage.get(City, city_id)
+
+    if my_cities_obj is not None:
+        my_json = request.get_json(silent=True)
+        if my_json is not None:
+            if "user_id" in my_json:
+                my_user_obj = storage.get(User, my_json['user_id'])
+                if my_user_obj is not None:
+                    if "name" in my_json:
+                        name = my_json["name"]
+                        new_p = Place(name=name, user_id=my_json['city_id'],
+                                      city_id=city_id)
+                new_p.save()
+                return make_response(jsonify(new_p.to_dict()), 201)
+            else:
+                abort(400, "Missing name")
+        else:
+            abort(400, "Not a JSON")
+    else:
+        abort(404)
+
+@app_views.route("/places/<place_id>", methods=["PUT"])
+def place_update(place_id=None):
+    """Updates a places from a dict"""
+    my_place_obj = storage.get(Place, place_id)
+
+    if my_place_obj is not None:
+        update_dict = request.get_json(silent=True)
+        if update_dict is not None:
+            for key, value in update_dict.items():
+                setattr(my_place_obj, key, value)
+                my_place_obj.save()
+            return make_response(jsonify(my_place_obj.to_dict()), 200)
+        else:
+            abort(400, "Not a JSON")
+    else:
+        abort(404)
